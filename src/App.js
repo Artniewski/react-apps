55c2f8d838a94f4434483d29e65f74d5cf544d57
artniewski <artniewski@gmail.com>
2022-02-18 18:35:21 +0100

    Add re-usable component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import Todo from "./components/Todo";
 
 function App() {
     return (
-        <div>Hello</div>
+        <div>
+            <h1>My Todos</h1>
+            <Todo text='Learn React' />
+            <Todo text='Second Card' />
+            <Todo text='Last One' />
+        </div>
     );
 }
 
